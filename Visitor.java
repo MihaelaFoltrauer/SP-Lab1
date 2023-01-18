bb88852c6b25f663ab952199bb94d8ed57b13649
MihaelaFoltrauer <92172425+MihaelaFoltrauer@users.noreply.github.com>
2023-01-19 01:13:13 +0200

    Delete Visitor.java

--- Visitor.java
+++
@@ -1,9 +0,0 @@
-public interface Visitor {
-    void visitBook(Book book);
-    void visitSection(Section section);
-    void visitTableOfContents(TableOfContents tableOfContents);
-    void visitParagraph(Paragraph paragraph);
-    void visitImageProxy(ImageProxy imageProxy);
-    void visitImage(Image image);
-    void visitTable(Table table);
-}

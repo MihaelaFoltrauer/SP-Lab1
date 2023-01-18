f10b26698ab4378a807fd18d14e2b472a26eb3bd
MihaelaFoltrauer <92172425+MihaelaFoltrauer@users.noreply.github.com>
2023-01-19 01:10:33 +0200

    Delete Element.java

--- Element.java
+++
@@ -1,8 +0,0 @@
-public interface Element {
-    public void print();
-    public void add(Element e);
-    public void remove(Element e);
-    public Element get(int index);
-    public void accept(Visitor visitor);
-}
-
